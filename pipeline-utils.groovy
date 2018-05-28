5579dc0eed5362d7d8af0c3644638486d1dacfb3
Jonathan Lebon <jonathan@jlebon.com>
2018-05-28 13:54:35 -0400

    Define parameters in Jenkins pipeline
    Rather than requiring parameters to be set on the Jenkins job where the
    pipeline is running, make it part of the definition here. This makes
    their definitions here even more self-contained. To do this, I moved all
    the internal strings into credentials that are then mapped to Groovy
    variables when needed. (And just hardcoded a few less sensitive ones).
    
    There's a gotcha I ran into early on, which is that on the first run,
    parameters are not set yet so environment variables referencing them
    would not work. I added a blurb about this in `pipeline-utils.groovy`.
    
    We should also be able to also define the timer syntax using this
    approach, though let's make sure this works smoothly first.

--- pipeline-utils.groovy
+++ pipeline-utils.groovy
@@ -6,6 +6,58 @@
 // let's try not to use env vars here to keep things
 // decoupled and easier to grok
 
+def define_parameters() {
+    /* There's a subtle gotcha here. Don't use `env.$PARAM`, but `params.$PARAM`
+     * instead. The former will *not* be set on the first run, since the
+     * parameters are not set yet. The latter will be set on the first run as
+     * soon as the below is executed. See:
+     * https://issues.jenkins-ci.org/browse/JENKINS-40574 */
+    properties([
+      parameters([
+        credentials(name: 'ARTIFACT_SERVER',
+                    credentialType: 'org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl',
+                    description: "Server used to push/receive built artifacts.",
+                    defaultValue: 'c051c78a-7210-4dec-92de-6f51616aac79',
+                    required: true),
+        credentials(name: 'ARTIFACT_SSH_CREDS_ID',
+                    credentialType: 'com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey',
+                    description: "SSH key for artifact server.",
+                    defaultValue: 'a5990862-8650-411c-9c19-049ee09344e5',
+                    required: true),
+        credentials(name: 'AWS_CREDENTIALS',
+                    credentialType: 'com.cloudbees.jenkins.plugins.awscredentials.AWSCredentialsImpl',
+                    description: "AWS credentials.",
+                    defaultValue: 'd08c733e-63e0-48f4-a2c4-4e060068f94e',
+                    required: true),
+        credentials(name: 'S3_PRIVATE_BUCKET',
+                    credentialType: 'org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl',
+                    description: "Private S3 bucket to use when uploading AMIs.",
+                    defaultValue: '5c9571a2-c492-421f-b506-ba469afffc10',
+                    required: true),
+        credentials(name: 'REGISTRY',
+                    credentialType: 'org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl',
+                    description: "Docker registry to which to push built container images.",
+                    defaultValue: '5e2e8032-25ad-4387-b89d-0d5a2bb0a3cd',
+                    required: true),
+        credentials(name: 'REGISTRY_CREDENTIALS',
+                    credentialType: 'com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl',
+                    description: "Credentials for Docker registry.",
+                    defaultValue: '46060330-e169-4fd9-a135-64d92a52968a',
+                    required: true),
+        credentials(name: 'OSTREE_INSTALL_URL',
+                    credentialType: 'org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl',
+                    description: "Remote OSTree repo URL to install from when running imagefactory.",
+                    defaultValue: '2d6637ef-7f53-4ee2-bd35-7865908560c7',
+                    required: true),
+        credentials(name: 'INSTALLER_TREE_URL',
+                    credentialType: 'org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl',
+                    description: "Local installer tree mirror to use when running imagefactory.",
+                    defaultValue: '50db8fac-f9d8-44e1-af0f-be29325a2896',
+                    required: true),
+      ])
+    ])
+}
+
 def rsync_dir_in(server, key, dir) {
     rsync_dir(key, "${server}:${dir}", dir)
 }
