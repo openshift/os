9b5706b2357e89e0d6197c9cbf25371ff32e628b
Colin Walters <walters@verbum.org>
2018-11-02 09:59:49 -0400

    Remove v1 pipeline
    This has moved to an internal repository.

--- .prow.sh
+++ .prow.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 set -xeuo pipefail
-
-# This script is run by prow using the coreos-assembler container.
-
-make syntax-check
-# And disable the container again until we figure out about Prow -> internal
-# make container
+# Nothing for now
