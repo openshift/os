4aaaedab8b6063088d3dc6f54556370898083a5c
Sohan Kunkerkar <sohank2602@gmail.com>
2020-11-30 17:28:52 -0500

    05rhcos/*: rhcos check for legacy LUKS configuration
    This change ensures that the user provides the coorect LUKS
    configuration in an Ignition config to enable LUKS in RHCOS,
    so they don't accidently end up with an unencrypted system.

---
+++ overlay.d/05rhcos/usr/lib/dracut/modules.d/41rhcos-check-luks-syntax/module-setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+install_unit() {
+    local unit="$1"; shift
+    local target="${1:-ignition-complete.target}"; shift
+    local instantiated="${1:-$unit}"; shift
+    inst_simple "$moddir/$unit" "$systemdsystemunitdir/$unit"
+    mkdir -p "$initdir/$systemdsystemunitdir/$target.requires"
+    ln_r "../$unit" "$systemdsystemunitdir/$target.requires/$instantiated"
+}
+
+install() {
+    inst_script "$moddir/rhcos-fail-boot-for-legacy-luks-config" \ 
+        "/usr/libexec/rhcos-fail-boot-for-legacy-luks-config"
+    
+    install_unit rhcos-fail-boot-for-legacy-luks-config.service
+}
