dec43de7a7ac0fe283b07b6b5f51115af7d58174
Colin Walters <walters@verbum.org>
2020-12-08 22:56:02 +0000

    rhcos-check-luks: Fix trailing space and script variable
    The trailing space causes boot failure in the initramfs because the script
    is missing.  Also the variable name was incorrect.

--- overlay.d/05rhcos/usr/lib/dracut/modules.d/41rhcos-check-luks-syntax/module-setup.sh
+++ overlay.d/05rhcos/usr/lib/dracut/modules.d/41rhcos-check-luks-syntax/module-setup.sh
@@ -10,7 +10,7 @@ install_unit() {
 }
 
 install() {
-    inst_script "$moddir/rhcos-fail-boot-for-legacy-luks-config" \ 
+    inst_script "$moddir/rhcos-fail-boot-for-legacy-luks-config" \
         "/usr/libexec/rhcos-fail-boot-for-legacy-luks-config"
     
     install_unit rhcos-fail-boot-for-legacy-luks-config.service
