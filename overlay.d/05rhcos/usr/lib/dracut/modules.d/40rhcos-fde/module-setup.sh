b4e51f23c204c8327ca4a4fbfaa7cea959b7a4c5
Jonathan Lebon <jonathan@jlebon.com>
2021-01-05 10:29:10 -0500

    Bump fedora-coreos-config for various fixes
    Specifically,
    - the ESP unraiding change (https://github.com/coreos/fedora-coreos-config/pull/794)
    - working around buggy udev (https://github.com/coreos/fedora-coreos-config/pull/779)
    - --copy-network regression (https://github.com/coreos/fedora-coreos-config/pull/800)
    - Azure udev rules in initramfs (https://github.com/coreos/fedora-coreos-config/pull/786)
    
    For the latter, also apply the same `|| exit 1` change from
    https://github.com/coreos/fedora-coreos-config/pull/800 to our own
    dracut modules for good measure.
    
    ```
    $ git shortlog --invert-grep --author='CoreOS Bot' 6c9f15c8857772f730cdb0b5b2df143e778c5d0d..8c07b7391473910ba3884ee0d3763743805ac78f
    Benjamin Gilbert (6):
          40ignition-ostree: silence mkfs.xfs
          coreos-boot-mount-generator: stop mounting /boot/efi
          40ignition-ostree: rename mount_and_restore_filesystem
          40ignition-ostree: create mountpoint in mount_verbose
          40ignition-ostree: add get_partlabels_for_parttype helper
          40ignition-ostree: copy ESP contents as independent filesystems
    
    Dusty Mabe (1):
          overrides: drop graduated overrides
    
    Jonathan Lebon (7):
          40ignition-ostree/transposefs: also trigger udev on by-label link mismatch for ESP
          40ignition-ostree/transposefs: factor out function to restore fs
          40ignition-ostree/transposefs: relabel the root of reprovisioned filesystems
          40ignition-ostree/transposefs: load zram with num_devices=0
          Add rawhide repo file
          05core: re-order and rename some dracut modules
          05core: add `|| exit 1` to `systemctl add-{requires,wants}` calls
    
    Kelvin Fan (1):
          overlay.d/15fcos: Stop utilizing c-l-h-m private dir
    
    Luca BRUNO (1):
          overrides: drop stale Afterburn entries
    
    Micah Abbott (2):
          use WALinuxAgent-udev package for Azure udev rules
          overlay: add new module for installing Azure udev rules
    
    Prashanth Sundararaman (1):
          tests: Enable TPM test for all arches except s390x
    
    Timothée Ravier (1):
          manifests/fedora-coreos-base: Mask systemd-repart
    ```

--- overlay.d/05rhcos/usr/lib/dracut/modules.d/40rhcos-fde/module-setup.sh
+++ overlay.d/05rhcos/usr/lib/dracut/modules.d/40rhcos-fde/module-setup.sh
@@ -8,7 +8,9 @@ install_unit() {
     local unit=$1; shift
     local target=${1:-ignition-complete.target}
     inst_simple "$moddir/$unit" "$systemdsystemunitdir/$unit"
-    systemctl -q --root="$initdir" add-requires "$target" "$unit"
+    # note we `|| exit 1` here so we error out if e.g. the units are missing
+    # see https://github.com/coreos/fedora-coreos-config/issues/799
+    systemctl -q --root="$initdir" add-requires "$target" "$unit" || exit 1
 }
 
 depends() {
