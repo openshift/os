d3d64c28e6cc239cc7699fdf4892f26fae74a7ef
Colin Walters <walters@verbum.org>
2021-01-21 20:10:33 +0000

    ci/build-test: Do a cosa build + test
    I think *finally* all the pieces are together for this.
    
    Closes: https://github.com/openshift/os/issues/417

--- ci/build-test.sh
+++ ci/build-test.sh
@@ -4,4 +4,17 @@ set -xeuo pipefail
 git submodule update --init --recursive
 # Basic syntax check
 ./fedora-coreos-config/ci/validate
-# To be expanded with a cosa build
+# Prow jobs don't support adding emptydir today
+export COSA_SKIP_OVERLAY=1
+gitdir=$(pwd)
+cd $(mktemp -d)
+cosa init ${gitdir}
+# TODO query the 4-7 bits from manifest.yaml or so
+curl -L http://base-4-7-rhel8.ocp.svc.cluster.local > src/config/ocp.repo
+cosa fetch
+cosa build
+cosa kola --basic-qemu-scenarios
+cosa kola run 'ext.*'
+# TODO: all tests in the future, but there are a lot
+# and we want multiple tiers, and we need to split them
+# into multiple pods and stuff.
