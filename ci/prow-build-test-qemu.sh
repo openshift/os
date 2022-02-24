4e8cc9ce925c10f8299fabb5ec6e8471390fa03c
Micah Abbott <miabbott@redhat.com>
2022-02-24 11:12:25 -0500

    ci: disable testiso scenarios
    All of the ISO-based `testiso` scenarios are failing due to https://github.com/openshift/os/issues/666
    so we are going to disable them until we get `util-linux` patched.
    
    See also: https://bugzilla.redhat.com/show_bug.cgi?id=2058176

--- ci/prow-build-test-qemu.sh
+++ ci/prow-build-test-qemu.sh
@@ -46,13 +46,17 @@ cosa buildextend-metal && cosa buildextend-metal4k && cosa buildextend-live
 # installs with the image format we ship
 cosa compress --artifact=metal --artifact=metal4k
 # Running testiso scenarios on metal artifact
-kola testiso -S --scenarios pxe-install,pxe-offline-install,iso-install,iso-offline-install,iso-live-login,iso-as-disk --output-dir tmp/kola-metal
+# Skip the following scenarios: iso-install,iso-offline-install,iso-live-login,iso-as-disk
+# See: https://github.com/openshift/os/issues/666
+kola testiso -S --scenarios pxe-install,pxe-offline-install --output-dir tmp/kola-metal
 # iso-install scenario to sanity-check the metal4k media
-kola testiso -S --qemu-native-4k --qemu-multipath --scenarios iso-install --output-dir tmp/kola-metal4k
-if [ $(uname -i) = x86_64 ] || [ $(uname -i) = aarch64 ]; then
-    mkdir -p tmp/kola-uefi
-    kola testiso -S --qemu-firmware uefi --scenarios iso-live-login,iso-as-disk --output-dir tmp/kola-uefi/insecure
-    if [ $(uname -i) = x86_64 ]; then
-        kola testiso -S --qemu-firmware uefi-secure --scenarios iso-live-login,iso-as-disk --output-dir tmp/kola-uefi/secure
-    fi
-fi
+# Skip all the testiso scenarios for metal4k + UEFI
+# See: https://github.com/openshift/os/issues/666
+# kola testiso -S --qemu-native-4k --qemu-multipath --scenarios iso-install --output-dir tmp/kola-metal4k
+# if [ $(uname -i) = x86_64 ] || [ $(uname -i) = aarch64 ]; then
+#     mkdir -p tmp/kola-uefi
+#     kola testiso -S --qemu-firmware uefi --scenarios iso-live-login,iso-as-disk --output-dir tmp/kola-uefi/insecure
+#     if [ $(uname -i) = x86_64 ]; then
+#         kola testiso -S --qemu-firmware uefi-secure --scenarios iso-live-login,iso-as-disk --output-dir tmp/kola-uefi/secure
+#     fi
+# fi
