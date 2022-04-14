1853cf1b1f7a83b876d46714b42cb13585c85db1
Zack Zlotnik <zzlotnik@redhat.com>
2022-04-14 11:44:36 -0400

    predictable path for OCI archive

--- ci/prow-build.sh
+++ ci/prow-build.sh
@@ -48,3 +48,9 @@ cosa buildfetch --url=${prev_build_url}
 cosa fetch
 cosa build
 cosa buildextend-extensions
+
+# Give the newly-built OCI archive a predictable filename to make OCI archive extraction simpler
+arch="x86_64"
+cosa_build_id="$(cat "${COSA_DIR}/builds/builds.json" | jq -r '.builds[0].id')"
+current_build_dir="${COSA_DIR}/builds/latest/${arch}"
+mv "${current_build_dir}/rhcos-${cosa_build_id}-ostree.${arch}.ociarchive" "${current_build_dir}/rhcos.${arch}.ociarchive"
