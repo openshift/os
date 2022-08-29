e9741465f35ac9e38352d4fc35176f4d8c4fe7fa
Colin Walters <walters@verbum.org>
2022-08-29 09:53:20 -0400

    overlay: Add `rhcos-selinux-policy-upgrade.service`
    This will auto rebuild the policy if we detect local modifications;
    it is only needed before RHEL 8.7 when we'll pick up the
    combined underlying work from
    https://bugzilla.redhat.com/show_bug.cgi?id=2057497

---
+++ tests/kola/rebuild-selinux-policy/test.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Test for https://issues.redhat.com/browse/OCPBUGS-595
+
+set -xeuo pipefail
+
+. $KOLA_EXT_DATA/commonlib.sh
+
+cd $(mktemp -d)
+journalctl -b -u rhcos-selinux-policy-upgrade > logs.txt
+RHEL_VERSION=$(. /usr/lib/os-release && echo ${RHEL_VERSION:-})
+echo "RHEL_VERSION=${RHEL_VERSION:-}"
+service_should_start=0
+case "${RHEL_VERSION:-}" in
+  8.[0-6]) service_should_start=1;;
+  *) ;;
+esac
+
+case "${AUTOPKGTEST_REBOOT_MARK:-}" in
+    "")
+    if grep -qFe 'Recompiling policy' logs.txt; then
+        cat logs.txt
+        fatal "Recompiled policy on first boot"
+    fi
+    setsebool -P container_manage_cgroup on
+    /tmp/autopkgtest-reboot changed-policy
+    ;;
+    "changed-policy") 
+    if test "${service_should_start}" = "1" && ! grep -qFe 'Recompiling policy' logs.txt; then
+        cat logs.txt
+        fatal "Failed to recompile policy on first boot"
+    fi
+    ;;
+esac
+echo ok
