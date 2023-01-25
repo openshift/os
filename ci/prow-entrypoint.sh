eb4f5eec3f245bf5ee69e254e227fa4160ac7e81
Timothée Ravier <tim@siosm.fr>
2023-01-25 11:54:45 +0100

    scos: Only pull packages from RHCOS repos by default
    Public builds and the OKD SCOS pipeline should use other repos.

--- ci/prow-entrypoint.sh
+++ ci/prow-entrypoint.sh
@@ -90,7 +90,7 @@ prepare_repos() {
         # Temporary workaround until we have all packages for SCOS
         curl --fail -L "http://base-${ocpver_mut}-rhel90.ocp.svc.cluster.local" -o "src/config/tmp.repo"
         awk '/rhel-9.0-server-ose-4.13/,/^$/' "src/config/tmp.repo" > "src/config/ocp90.repo"
-        echo "includepkgs=openshift-clients,openshift-hyperkube" >> "src/config/ocp90.repo"
+        echo "includepkgs=openshift-clients,openshift-hyperkube,cri-o,conmon-rs" >> "src/config/ocp90.repo"
         rm "src/config/tmp.repo"
     fi
 }
