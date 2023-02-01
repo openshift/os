d14ed6f1e2950dddc673bfd14d33de9c85c2df8a
Colin Walters <walters@verbum.org>
2023-02-01 16:38:06 -0500

    ci/get-ocp-repo.sh: Run verbosely
    For ease of debugging.

--- ci/get-ocp-repo.sh
+++ ci/get-ocp-repo.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # TODO: share this with ci/prow-entrypoint.sh
+set -euo pipefail
 ocpver=$(rpm-ostree compose tree --print-only manifest.yaml | jq -r '.["mutate-os-release"]')
 ocpver_mut=$(rpm-ostree compose tree --print-only manifest.yaml | jq -r '.["mutate-os-release"]' | sed 's|\.|-|')
 rhelver=$(rpm-ostree compose tree --print-only manifest.yaml | jq -r '.["automatic-version-prefix"]' | cut -f2 -d.)
 
+set -x
 curl --fail -L "http://base-${ocpver_mut}-rhel${rhelver}.ocp.svc.cluster.local" -o "ocp.repo"
+cat ocp.repo
