d7601ecb100445086e8ef5a9b87e2c92484045f1
Jonathan Lebon <jonathan@jlebon.com>
2024-04-09 22:06:24 -0400

    ci: query the variant-specific manifest for centos-stream-release package
    Otherwise, we'll just always be querying the default one, which might
    not be stream-based.

--- ci/prow-entrypoint.sh
+++ ci/prow-entrypoint.sh
@@ -122,6 +122,10 @@ kola_test_qemu() {
     # we need to check if the basic.nvme is available in the list of tests
     local args=""
     local manifest="src/config/manifest.yaml"
+    if [[ -f "src/config.json" ]]; then
+        variant="$(jq --raw-output '."coreos-assembler.config-variant"' 'src/config.json')"
+        manifest="src/config/manifest-${variant}.yaml"
+    fi
     if cosa kola list --json | jq -r '.[].Name' | grep -q "basic.nvme"; then
         if rpm-ostree compose tree --print-only "${manifest}" | jq -r '.packages[]' | grep -q "centos-stream-release"; then
             args+="--denylist-test *.uefi-secure"
