da76f64b1fc822ee6f9ca16497022448a734fde2
jbtrystram <jbtrystram@redhat.com>
2025-05-12 13:56:35 +0200

    extensions: llvm now available in rhel 9.6 and 10
    Closes https://github.com/openshift/os/issues/1680

--- ci/get-ocp-repo.sh
+++ ci/get-ocp-repo.sh
@@ -8,11 +8,6 @@ set -euo pipefail
 urls=(
     # theoretically that's the only one we need
     "http://base-4-19-rhel96.ocp.svc.cluster.local"
-    # XXX: but also currently add 9.4 repos for crun-wasm when building extensions
-    # https://github.com/openshift/os/issues/1680
-    # https://github.com/openshift/os/pull/1682
-    # https://issues.redhat.com/browse/COS-3075
-    "http://base-4-19-rhel94.ocp.svc.cluster.local"
 )
 
 dest=$1; shift
